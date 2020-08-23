d9ba0e709f67965ff5df2027f083facea0a33e31
div-div <numatachiakii@gmail.com>
2020-08-23 22:03:10 +0900

    enable to post movies  and function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,15 +1,28 @@
 class MoviesController < ApplicationController
+  # before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @movies = Movie.all.order("id DESC")
+    
   end
 
   def new
     @movie = Movie.new
   end
 
+  def show
+    @movie = Movie.find_by(id: params[:id])
+    @user = User.find_by(id: @movie.user_id)
+  # binding.pry
+
+  end
+
   def create
     @movie = Movie.new(movie_params)
-    if @movie.save
-      render 'index'
+    @movie.user_id = current_user.id
+    if @movie.save!
+
+      redirect_to root_path
     else
       render 'new'
     end
@@ -17,6 +30,7 @@ class MoviesController < ApplicationController
 
 private
   def movie_params
-    params.require(:movie).permit(:title, :body, :image_url)
+    params.require(:movie).permit(:title, :body, :image).merge(user_id: current_user.id)
   end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+
 end
