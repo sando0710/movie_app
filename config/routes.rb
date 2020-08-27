1e29e89dfdad83cda197be390412099d2e2b8723
div-div <numatachiakii@gmail.com>
2020-08-27 14:49:35 +0900

    category function-1

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   root 'movies#index'
 
   resources :movies do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
     resources :comments
-
   end
 end
